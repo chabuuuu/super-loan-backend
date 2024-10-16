0f8910f5722c4adbb7564724e40d970c67d98e32
chabuuuu <haphuthinh332004@gmail.com>
2024-10-16 13:15:32 +0700

    fix(ci pipeline): add enviroment

--- super-loan.loan-service/main/src/database/db.datasource.ts
+++ super-loan.loan-service/main/src/database/db.datasource.ts
@@ -14,11 +14,7 @@ export class AppDataSourceSingleton {
     if (!AppDataSourceSingleton.instance) {
       AppDataSourceSingleton.instance = new DataSource({
         type: 'postgres',
-        host: process.env.DB_HOST || 'localhost',
-        port: Number(process.env.DB_PORT) || 5432,
-        username: process.env.DB_USERNAME || 'postgres',
-        password: process.env.DB_PASSWORD || 'admin',
-        database: process.env.DB_NAME || 'test',
+        url: process.env.DATABASE_URL || 'postgres://username:password@host:port/database',
         entities: [Account, Role],
         synchronize: GlobalConfig.database.sync,
         logging: true,
