be2f90e183506904aa83cf3ea3d785ee3c267524
Nam1834 <nam.2174802010895@vanlanguni.vn>
2024-10-25 15:02:39 +0700

    feat(fix login): fix login validate phoneNumber

--- super-loan.loan-service/main/src/dto/borrower/login-borrower.req.ts
+++ super-loan.loan-service/main/src/dto/borrower/login-borrower.req.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsString, IsStrongPassword, MinLength, ValidateIf, MaxLengt
 export class LoginBorrowerReq {
   @IsNotEmpty()
   @IsString()
-  @Matches(/^(0[235789][0-9]{8}|[\w\-.]+@[a-zA-Z\d\-.]+\.[a-zA-Z]{2,4})$/, {
+  @Matches(/^(0(3[2-9]|5[6|8|9]|7[0|6-9]|8[1-9]|9[0-9])[0-9]{7,8}|[\w\-.]+@[a-zA-Z\d\-.]+\.[a-zA-Z]{2,4})$/, {
     message: 'Must be a valid email or Vietnamese phone number'
   })
   emailOrPhoneNumber!: string;
@@ -15,7 +15,7 @@ export class LoginBorrowerReq {
   @MaxLength(12, { message: 'Password must not exceed 12 characters' })
   password!: string;
 
-  @IsNotEmpty()
-  @IsString()
-  captchaToken!: string;
+  // @IsNotEmpty()
+  // @IsString()
+  // captchaToken!: string;
 }
