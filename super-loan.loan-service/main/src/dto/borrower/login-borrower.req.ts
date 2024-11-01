f5ced1d57265877eb59739ee15bdd7fb5a2138cc
chabuuuu <haphuthinh332004@gmail.com>
2024-11-01 13:51:40 +0700

    build(multi language): finish

--- super-loan.loan-service/main/src/dto/borrower/login-borrower.req.ts
+++ super-loan.loan-service/main/src/dto/borrower/login-borrower.req.ts
@@ -1,18 +1,19 @@
 import { IsNotEmpty, IsString, IsStrongPassword, MinLength, ValidateIf, MaxLength, Matches } from 'class-validator';
+import i18n from 'i18n';
 
 export class LoginBorrowerReq {
-  @IsNotEmpty()
-  @IsString()
+  @IsNotEmpty({ message: () => i18n.__('IS_NOT_EMPTY') })
+  @IsString({ message: () => i18n.__('IS_STRING') })
   @Matches(/^(0(3[2-9]|5[6|8|9]|7[0|6-9]|8[1-9]|9[0-9])[0-9]{7,8}|[\w\-.]+@[a-zA-Z\d\-.]+\.[a-zA-Z]{2,4})$/, {
-    message: 'Must be a valid email or Vietnamese phone number'
+    message: () => i18n.__('IS_VALID_PHONE')
   })
   emailOrPhoneNumber!: string;
 
-  @IsNotEmpty()
-  @IsString()
+  @IsNotEmpty({ message: () => i18n.__('IS_NOT_EMPTY') })
+  @IsString({ message: () => i18n.__('IS_STRING') })
   @IsStrongPassword()
-  @MinLength(6, { message: 'Password must be at least 6 characters' })
-  @MaxLength(12, { message: 'Password must not exceed 12 characters' })
+  @MinLength(6, { message: () => i18n.__('IS_MIN_6') })
+  @MaxLength(12, { message: () => i18n.__('IS_MAX_12') })
   password!: string;
 
   // @IsNotEmpty()
