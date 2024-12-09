8e5ccebc2f71bc9665db15a953075b0166a6a08e
chabuuuu <haphuthinh332004@gmail.com>
2024-12-09 14:10:05 +0700

    fix(validate error): error constant

--- super-loan.loan-service/main/src/constants/validate.constants.ts
+++ super-loan.loan-service/main/src/constants/validate.constants.ts
@@ -1,10 +1,4 @@
 export class ValidateError {
-  public static readonly NOT_EMAIL = 'INVALID_EMAIL';
-  public static readonly NOT_PHONE_NUMBER = 'NOT_PHONE_NUMBER';
-  public static readonly FULLNAME_CONTAIN_SPECIAL_CHARS = 'FULLNAME_CONTAIN_SPECIAL_CHARS';
-  public static readonly FULLNAME_LEADING_OR_TRAINLING_SPACE = 'FULLNAME_LEADING_OR_TRAINLING_SPACE';
-  public static readonly NOT_STRING = 'NOT_STRING';
-  public static readonly NOT_DATE_STRING = 'NOT_DATE_STRING';
-  public static readonly NOT_STRONG_PASSWORD = 'NOY_STRONG_PASSWORD';
   public static readonly NOT_EMPTY = 'NOT_EMPTY';
+  public static readonly INVALID = 'INVALID';
 }
