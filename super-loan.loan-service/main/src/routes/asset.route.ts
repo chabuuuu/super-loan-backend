b2256fef06eea2576e420bbb4c3573e41f0acb64
chabuuuu <haphuthinh332004@gmail.com>
2024-12-12 15:56:22 +0700

    feat(check permission): add middleware

--- super-loan.loan-service/main/src/routes/asset.route.ts
+++ super-loan.loan-service/main/src/routes/asset.route.ts
@@ -1,14 +1,29 @@
+import { Permissions } from '@/constants/permission.constants';
 import { assetController } from '@/container/asset.container';
 import { AssetCreateReq } from '@/dto/asset/asset-create.req';
 import { AssetUpdateReq } from '@/dto/asset/asset-update.req';
+import { authenticateJWT } from '@/middleware/authenticate.middelware';
+import { checkPermission } from '@/middleware/check-permission.middleware';
 import { classValidate } from '@/middleware/class-validate.middleware';
 import express from 'express';
 const assetRouter = express.Router();
 
 assetRouter
 
-  .put('/:id', classValidate(AssetUpdateReq), assetController.update.bind(assetController))
-  .post('/', classValidate(AssetCreateReq), assetController.common.create.bind(assetController.common))
+  .put(
+    '/:id',
+    authenticateJWT,
+    checkPermission([Permissions.QUAN_LY_TAI_SAN]),
+    classValidate(AssetUpdateReq),
+    assetController.update.bind(assetController)
+  )
+  .post(
+    '/',
+    authenticateJWT,
+    checkPermission([Permissions.QUAN_LY_TAI_SAN]),
+    classValidate(AssetCreateReq),
+    assetController.common.create.bind(assetController.common)
+  )
   .get('/paging', assetController.common.findWithPaging.bind(assetController.common))
   .get('/:id', assetController.findOne.bind(assetController))
   .get('/', assetController.common.findAll.bind(assetController.common));
--- super-loan.loan-service/main/src/routes/loan_package.route.ts
+++ super-loan.loan-service/main/src/routes/loan_package.route.ts
@@ -1,10 +1,19 @@
+import { Permissions } from '@/constants/permission.constants';
 import { loanPackageController } from '@/container/loan_package.container';
 import { CreateLoanPackageReq } from '@/dto/loan_package/create-loan-package.req';
+import { authenticateJWT } from '@/middleware/authenticate.middelware';
+import { checkPermission } from '@/middleware/check-permission.middleware';
 import { classValidate } from '@/middleware/class-validate.middleware';
 import express from 'express';
 const loanPackageRouter = express.Router();
 loanPackageRouter
-  .post('/create', classValidate(CreateLoanPackageReq), loanPackageController.createPackage.bind(loanPackageController))
+  .post(
+    '/create',
+    authenticateJWT,
+    checkPermission([Permissions.QUAN_LY_GOI_VAY]),
+    classValidate(CreateLoanPackageReq),
+    loanPackageController.createPackage.bind(loanPackageController)
+  )
   .get('/get-list', loanPackageController.getAll.bind(loanPackageController))
   .get('/get-detail/:id', loanPackageController.getLoanPackageDetail.bind(loanPackageController));
 
